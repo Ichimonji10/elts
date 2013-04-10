b06af4bb62b025f2848c2952c52c7db31095759b
Jeremy Audet <ichimonji10@gmail.com>
2013-04-10 14:40:10 +0000

    Update docstrings
    git-svn-id: https://subversion.assembla.com/svn/elts@10 3fefdeda-3626-427b-91b0-c7b7be374f37

--- trunk/lending/models.py
+++ trunk/lending/models.py
@@ -1,3 +1,4 @@
+"""Data models at the core of ELTS."""
 from django.db import models
 
 # Model reference: https://docs.djangoproject.com/en/dev/ref/models/fields/
@@ -22,16 +23,19 @@ from django.db import models
 # It is both common and OK for a model to have no __init__ method.
 
 class Item(models.Model):
+    """An item which can be lent out to a person."""
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 50, db_index = True)
     description = models.TextField(blank = True)
     due_back_date = models.DateField(blank = True)
 
 class ItemTag(models.Model):
+    """Junction table relating items and tags."""
     item_id = models.ForeignKey('Item', to_field = 'id', primary_key = True)
     tag_id = models.ForeignKey('Tag',  to_field = 'id', primary_key = True)
 
 class Lend(models.Model):
+    """Tracks the lending of an item to a person."""
     id = models.AutoField(primary_key=True)
     datetime_out = models.DateTimeField()
     datetime_in = models.DateTimeField(blank = True)
@@ -39,6 +43,7 @@ class Lend(models.Model):
     person_ad_guid = models.ForeignKey('Person', to_field = 'ad_guid')
 
 class Reservation(models.Model):
+    """Reserves the lending of an item to a person in the future."""
     id = models.AutoField(primary_key=True)
     date_in = models.DateField()
     date_out = models.DateField()
@@ -46,6 +51,7 @@ class Reservation(models.Model):
     person_ad_guid = models.ForeignKey('Person', to_field = 'ad_guid')
 
 class Note(models.Model):
+    """An arbitrary, descriptive note about an item."""
     id = models.AutoField(primary_key=True)
     note_date = models.DateTimeField()
     note_text = models.TextField()
@@ -53,10 +59,20 @@ class Note(models.Model):
     person_ad_guid = models.ForeignKey('Person', to_field = 'ad_guid')
 
 class Tag(models.Model):
+    """A categorization for an item. (e.g. "laptop")"""
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length = 30, unique = True) # implies db_index
 
 class Person(models.Model):
+    """A person.
+
+    Items can be lent to or reserved for a person.
+
+    The canonical source of information about people is an active directory
+    server. However, for performance reasons, some information should be
+    cached. That information is cached here.
+
+    """
     ad_guid = models.CharField(max_length = 16, primary_key = True)
     full_name = models.CharField(max_length = 50, blank = True)
     email = models.CharField(max_length = 50, blank = True)
