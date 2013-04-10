fa97f7ddd598b63303b26133ef4c4113091fc6eb
Jeremy Audet <ichimonji10@gmail.com>
2013-04-10 19:54:22 +0000

    Flesh out URL structure of ELTS app.
    The fleshed-out URL structure isn't 100% done, but it's significantly further
    than it was. The comments explaining the URL structure were moved from regular
    python comments into module-wide python docstrings.
    
    
    git-svn-id: https://subversion.assembla.com/svn/elts@15 3fefdeda-3626-427b-91b0-c7b7be374f37

--- trunk/elts/urls.py
+++ trunk/elts/urls.py
@@ -1,19 +1,43 @@
+"""URLs for the ELTS application.
+
+Sample URLs and explanations follow.
+
+/
+    The default index page. At the very least, it should show which items are
+    going out and due back in today.
+
+/item
+/item?tag=laptop
+/item?name=macbook
+    A list of all items. It is possible to search for specific items from this
+    URL. For example, `/item?tag=laptop` would display items with the tag
+    "laptop".
+
+/item/1
+    Details about the item with an ID of 1. Some information can be edited from
+    here, e.g. adding a new note about the item. However, not all fields can be
+    edited.
+
+/item/1/edit
+    Details about the item with an ID of 1, plus the ability to edit that
+    information.
+
+/tag
+    A place to create, read (view), update (rename), and delete tags.
+
+/reservation
+    TODO: flesh this out more
+
+/lend
+    TODO: flesh this out more
+
+/calendar
+    A page which displays all reservations and lends on a calendar. TODO: flesh
+    this out more
+
+"""
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'elts.views.index'),
-
-    # READ items
-    # * links for CRUD-ing each item
-    #
-    # READ item
-    # * tools for CRUD-ing notes
-    #
-    # READ tags
-    # * links for CRUD-ing each tag
-    #
-    # READ lends and reservations
-    # * links for CRU-ing each lend
-    # * links for CRUD-ing each reservation
-    # * display lends and reservations using a calendar?
 )
