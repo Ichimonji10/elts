4c78cba3c602997c4134eaf5b0790e2e8b81af2c
Jeremy Audet <ichimonji10@gmail.com>
2013-04-10 18:09:28 +0000

    Add base.html template for lending app and use it
    git-svn-id: https://subversion.assembla.com/svn/elts@12 3fefdeda-3626-427b-91b0-c7b7be374f37

--- trunk/lending/views.py
+++ trunk/lending/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django import http, template
 
 def index(request):
-    return HttpResponse('lending.views.index')
+    tplate = template.loader.get_template('lending/base.html')
+    ctext = template.RequestContext(
+        request,
+        {
+            'title': 'some random title',
+            'body': 'body for file',
+        }
+    )
+    return http.HttpResponse(tplate.render(ctext))
