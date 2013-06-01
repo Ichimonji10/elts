a8f6cba0b9d0cd891f17a66331bc0c6ac11fd42e
Jeremy Audet <ichimonji10@gmail.com>
2013-06-01 21:30:25 +0000

    Refactor template tags
    The template tags ``item_tags_as_links`` and ``tag_items_as_links`` have been
    removed, and ``tag_link`` and ``item_link`` have been added. The new template
    tags are simpler, and they chain together nicely. For example:
    
        <td>
            {% for tag in item|item_tags %}
                {{ tag|tag_link|safe }}
            {% endfor %}
        </td>
    
    
    git-svn-id: https://subversion.assembla.com/svn/elts@69 3fefdeda-3626-427b-91b0-c7b7be374f37

--- trunk/code/elts/templatetags/tag_tools.py
+++ trunk/code/elts/templatetags/tag_tools.py
@@ -11,29 +11,21 @@ def item_tags(item):
     return models.Tag.objects.filter(item__id = item.id)
 
 @register.filter
-def item_tags_as_links(item):
-    """Returns ``item``'s tags as a comma-separated, hyperlinked list.
+def tag_link(tag):
+    """Returns URI for the given ``tag``.
 
-    Each element of the returned list is an HTML <a> tag. For example:
+    Sample output:
 
-        [
-            '<a href="/elts/tag/15">daft</a>',
-            '<a href="/elts/tag/20">furry</a>',
-        ]
+        '<a href="/elts/tag/15">daft</a>'
 
     """
-    tags = models.Tag.objects.filter(item__id = item.id)
-    return [
-        '<a href="{}">{}</a>'.format(
-            urlresolvers.reverse(
-                'elts.views.tag_id',
-                args = [tag.id],
-            ),
-            tag.name,
-        )
-        for tag
-        in tags
-    ]
+    return '<a href="{}">{}</a>'.format(
+        urlresolvers.reverse(
+            'elts.views.tag_id',
+            args = [tag.id],
+        ),
+        tag.name,
+    )
 
 @register.filter
 def tag_items(tag):
@@ -41,26 +33,18 @@ def tag_items(tag):
     return models.Item.objects.filter(tags__id = tag.id)
 
 @register.filter
-def tag_items_as_links(tag):
-    """Returns ``tag``'s items as a comma-separated, hyperlinked list.
+def item_link(item):
+    """Returns URI for the given ``item``.
 
-    Each element of the returned list is an HTML <a> tag. For example:
+    Sample output:
 
-        [
-            '<a href="/elts/item/15">sportscar</a>',
-            '<a href="/elts/item/20">penguin</a>',
-        ]
+        '<a href="/elts/item/15">daft</a>'
 
     """
-    items = models.Item.objects.filter(tags__id = tag.id)
-    return [
-        '<a href="{}">{}</a>'.format(
-            urlresolvers.reverse(
-                'elts.views.item_id',
-                args = [item.id],
-            ),
-            item.name,
-        )
-        for item
-        in items
-    ]
+    return '<a href="{}">{}</a>'.format(
+        urlresolvers.reverse(
+            'elts.views.item_id',
+            args = [item.id],
+        ),
+        item.name,
+    )
