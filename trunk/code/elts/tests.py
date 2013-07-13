ba82d0f73b1f967bf8fe275ec0f54e2a0cc51a28
Jeremy Audet <ichimonji10@gmail.com>
2013-07-13 22:17:37 +0000

    Expand tests.py
    A framework for writing tests has been placed in ``tests.py``. This framework
    does not contain especially useful tests, but it will allow for a good, modular
    build-out of tests in the future. More to come...
    
    
    git-svn-id: https://subversion.assembla.com/svn/elts@92 3fefdeda-3626-427b-91b0-c7b7be374f37

--- trunk/code/elts/tests.py
+++ trunk/code/elts/tests.py
@@ -1,14 +1,29 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+"""Unit tests for this django app.
 
-Replace this with more appropriate tests for your application.
-"""
+``django.utils.unittest`` is an alias for Django's bundled copy of unittest2,
+backported for Python 2.5 compatibility. If you have Python2.7 (ergo unittest2)
+installed already, that will be used instead. Further, "``django.test.TestCase``
+[...] is a subclass of ``unittest.TestCase`` that runs each test inside a
+transaction to provide isolation".
 
+For details, see:
+https://docs.djangoproject.com/en/1.5/topics/testing/overview/#writing-tests
+
+"""
 from django.test import TestCase
+from django.utils.unittest import TestLoader, TestSuite
 
+class LoginTestCase(TestCase):
+    def test_get_login_page(self):
+        self.assertTrue(True)
+        # fetch login page with self.user, assert 200 return code, etc
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """Tests that 1 + 1 always equals 2."""
-        self.assertEqual(1 + 1, 2)
+class SampleTestCase(TestCase):
+    def test_addition(self):
+        self.assertEqual(2 + 2, 4)
+
+def suite():
+    return TestSuite([
+        TestLoader().loadTestsFromTestCase(LoginTestCase),
+        TestLoader().loadTestsFromTestCase(SampleTestCase),
+    ])
