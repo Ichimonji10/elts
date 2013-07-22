583161ffee2cf6484cb0d462c609a32f49994d3e
Jeremy Audet <ichimonji10@gmail.com>
2013-07-22 22:59:57 +0000

    Add some unit tests to GET various views.
    git-svn-id: https://subversion.assembla.com/svn/elts@99 3fefdeda-3626-427b-91b0-c7b7be374f37

--- trunk/code/elts/tests.py
+++ trunk/code/elts/tests.py
@@ -18,13 +18,13 @@ from elts import factories
 import doctest
 
 class LoginTestCase(TestCase):
-    def test_get_login_page(self):
+    def test_get_login(self):
         """GET the login view."""
         response = self.client.get(reverse('elts.views.login'))
         self.assertEqual(response.status_code, 200)
 
-    def test_post_login(self): # FIXME: add DELETE test
-        """POST and DELETE the login view."""
+    def test_post_login(self):
+        """POST the login view."""
         password = factories.random_utf8_str(20)
         user = factories.UserFactory.create(password = make_password(password))
         response = self.client.post(
@@ -32,6 +32,8 @@ class LoginTestCase(TestCase):
             {'username': user.username, 'password': password}
         )
         self.assertRedirects(response, reverse('elts.views.index'))
+        # FIXME: check for presence of user ID
+        # FIXME: add DELETE test
 
     def test_post_login_failure(self):
         """POST the login view, incorrectly."""
@@ -41,8 +43,41 @@ class LoginTestCase(TestCase):
         )
         self.assertRedirects(response, reverse('elts.views.login'))
 
+class IndexTestCase(TestCase):
+    def test_get_index(self):
+        """GET the index view."""
+        response = self.client.get(reverse('elts.views.index'))
+        self.assertEqual(response.status_code, 200)
+
+class CalendarTestCase(TestCase):
+    def test_get_calendar(self):
+        """GET the calendar view."""
+        response = self.client.get(reverse('elts.views.calendar'))
+        self.assertEqual(response.status_code, 200)
+
+class ItemTestCase(TestCase):
+    def test_get_item(self):
+        """GET the item view."""
+        response = self.client.get(reverse('elts.views.item'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_item_create_form(self):
+        """GET the item_create_form view."""
+        response = self.client.get(reverse('elts.views.item_create_form'))
+        self.assertEqual(response.status_code, 200)
+
+class TagTestCase(TestCase):
+    def test_get_tag(self):
+        """GET the tag view."""
+        response = self.client.get(reverse('elts.views.tag'))
+        self.assertEqual(response.status_code, 200)
+
 def suite():
     suite = unittest.TestSuite()
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(IndexTestCase))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(CalendarTestCase))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ItemTestCase))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(LoginTestCase))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TagTestCase))
     suite.addTest(doctest.DocTestSuite(factories))
     return suite
