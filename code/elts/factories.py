9b45cc9c599756d471645258a5c7124c7d81d5c9
Jeremy Audet <ichimonji10@gmail.com>
2013-11-22 15:29:33 -0500

    Update factories
    Break down factories in to smaller components. For example, class UserFactory
    now populates its `username` field using the `user_username()` function, and
    class ItemFactory populates its `name` field using the `item_name()` function.
    This change has two benefits:
    
    * The functions that generate values for individual fields can be tested
      individually. Formerly, the factory as a whole could be tested, but not
      individual fields.
    * Code in other modules can now use these functions. `test_forms.py` in
      particular will benefit from this change.

--- code/elts/factories.py
+++ code/elts/factories.py
@@ -18,7 +18,7 @@ the factory_boy implementation of timezone data is used. It's beautiful. See:
 * http://docs.python.org/2/library/datetime.html
 
 """
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from elts import models
@@ -28,62 +28,8 @@ from factory.django import DjangoModelFactory
 from factory.fuzzy import FuzzyAttribute, FuzzyDate, FuzzyDateTime
 import random
 
-# See ``_random_username`` for details on why this charset was chosen.
+# See ``user_username`` for details on why this charset was chosen.
 USERNAME_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_@+.-'
-
-def _random_username(prefix = ''):
-    """Generates a random username for a User.
-
-    For details on why usernames are composed from USERNAME_CHARSET, see:
-    https://docs.djangoproject.com/en/dev/ref/contrib/auth/#django.contrib.auth.models.User.username
-
-    >>> username = _random_username()
-    >>> len(username) in range(1, 31)
-    True
-    >>> username[0] in USERNAME_CHARSET
-    True
-    >>> username[-1] in USERNAME_CHARSET
-    True
-
-    """
-    username = str(prefix)
-    for i in range(random.randint(1, 30 - len(username))):
-        username += random.choice(USERNAME_CHARSET)
-    return username
-
-def _random_integer(lower, upper):
-    """Returns a random integer between ``lower`` and ``upper``, inclusive.
-
-    If ``lower >= upper``, returns ``lower``.
-
-    >>> _random_integer(0, 0)
-    0
-    >>> _random_integer(5, 5)
-    5
-    >>> _random_integer(5, 0)
-    5
-    >>> _random_integer(0, 5) in range(0, 6)
-    True
-
-    """
-    if(lower >= upper):
-        return lower
-    return random.randint(0, upper - lower) + lower
-
-def random_utf8_str(min_len = 0, max_len = 0):
-    """Returns a string consisting of random UTF-8 characters.
-
-    If ``min_len >= max_len``, returns a string exactly ``min_len`` characters
-    long. Otherwise, returns a string between ``min_len`` and ``max_len`` chars
-    long, inclusive.
-
-    See also: http://docs.python.org/2/library/functions.html#unichr
-
-    """
-    string = ''
-    for i in range(_random_integer(min_len, max_len)):
-        string += unichr(random.randrange(0, 65535))
-    return string
 
 class UserFactory(DjangoModelFactory):
     """Builds a ``django.contrib.auth.models.User`` object.
@@ -103,13 +49,37 @@ class UserFactory(DjangoModelFactory):
     # pylint: disable=R0903
     # pylint: disable=W0232
     FACTORY_FOR = User
-    username = Sequence(lambda n: _random_username(n))
-    password = FuzzyAttribute(lambda: make_password(random_utf8_str(1, 20)))
+    username = Sequence(lambda n: user_username(n)) # pylint: disable=W0108
+    password = FuzzyAttribute(lambda: user_password()) # pylint: disable=W0108
+
+def user_username(prefix = ''):
+    """Return a value suitable for the ``User.username`` model attribute.
+
+    For details on why usernames are composed from USERNAME_CHARSET, see:
+    https://docs.djangoproject.com/en/dev/ref/contrib/auth/#django.contrib.auth.models.User.username
+
+    >>> username = user_username()
+    >>> len(username) in range(1, 31)
+    True
+    >>> username[0] in USERNAME_CHARSET
+    True
+    >>> username[-1] in USERNAME_CHARSET
+    True
+
+    """
+    username = str(prefix)
+    for i in range(random.randint(1, 30 - len(username))):
+        username += random.choice(USERNAME_CHARSET)
+    return username
+
+def user_password():
+    """Return a value suitable for the ``User.password`` model attribute."""
+    return make_password(_random_utf8_str(1, 20))
 
 def create_user():
     """Build and save a User.
 
-    Returns an array of two objects: a User and a it's unencrypted password.
+    Return an array of two objects: a User and a it's unencrypted password.
 
     >>> user, password = create_user()
     >>> user.check_password(password)
@@ -121,9 +91,11 @@ def create_user():
     # This function promises that it will return an unencrypted password, but an
     # unencrypted password cannot be fetched from a User object. The solution is
     # to make an unencrypted password and _then_ create the User object.
-    password = random_utf8_str(1, 20)
+    password = _random_utf8_str(1, 20)
     user = UserFactory.create(password = make_password(password))  # pylint: disable=E1101
     return [user, password]
+
+#-------------------------------------------------------------------------------
 
 class ItemFactory(DjangoModelFactory):
     """Instantiate an ``elts.models.Item`` object.
@@ -136,7 +108,63 @@ class ItemFactory(DjangoModelFactory):
     # pylint: disable=R0903
     # pylint: disable=W0232
     FACTORY_FOR = models.Item
-    name = FuzzyAttribute(lambda: random_utf8_str(1, models.Item.MAX_LEN_NAME))
+    name = FuzzyAttribute(lambda: item_name()) # pylint: disable=W0108
+
+def item_name():
+    """Return a value suitable for the ``Item.name`` model attribute.
+
+    >>> from elts.models import Item
+    >>> name = item_name()
+    >>> isinstance(name, unicode)
+    True
+    >>> len(name) >= 1
+    True
+    >>> len(name) <= Item.MAX_LEN_NAME
+    True
+
+    """
+    return _random_utf8_str(1, models.Item.MAX_LEN_NAME)
+
+def item_description():
+    """Return a value suitable for the ``Item.description`` model attribute.
+
+    >>> from elts.models import Item
+    >>> description = item_description()
+    >>> isinstance(description, unicode)
+    True
+    >>> len(description) >= 1
+    True
+    >>> len(description) <= Item.MAX_LEN_DESCRIPTION
+    True
+
+    """
+    return _random_utf8_str(1, models.Item.MAX_LEN_DESCRIPTION)
+
+def item_tags():
+    """Return a value suitable for the ``Item.tags`` model attribute.
+
+    >>> tag_ids = item_tags()
+    >>> isinstance(tag_ids, list)
+    True
+
+    """
+    return [
+        # pylint: disable=E1101
+        TagFactory.create().id
+        for i
+        in range(0, random.randint(0, 2))
+    ]
+
+def item_is_lendable():
+    """Return a value suitable for the ``Item.is_lendable`` model attribute.
+
+    >>> item_is_lendable() in [True, False]
+    True
+
+    """
+    return random.choice([True, False])
+
+#-------------------------------------------------------------------------------
 
 class TagFactory(DjangoModelFactory):
     """Instantiate an ``elts.models.Tag`` object.
@@ -149,7 +177,65 @@ class TagFactory(DjangoModelFactory):
     # pylint: disable=R0903
     # pylint: disable=W0232
     FACTORY_FOR = models.Tag
-    name = FuzzyAttribute(lambda: random_utf8_str(models.Tag.MAX_LEN_NAME))
+    name = FuzzyAttribute(lambda: tag_name()) # pylint: disable=W0108
+
+def tag_name():
+    """Return a value suitable for the ``Tag.name`` model attribute.
+
+    >>> from elts.models import Tag
+    >>> name = tag_name()
+    >>> isinstance(name, unicode)
+    True
+    >>> len(name) >= 1
+    True
+    >>> len(name) <= Tag.MAX_LEN_NAME
+    True
+
+    """
+    return _random_utf8_str(1, models.Tag.MAX_LEN_NAME)
+
+def invalid_tag_name():
+    """Return a value unsuitable for the ``Tag.name`` model attribute.
+
+    >>> from elts.models import Tag
+    >>> name = invalid_tag_name()
+    >>> isinstance(name, unicode)
+    True
+    >>> len(name) == Tag.MAX_LEN_NAME + 1
+    True
+
+    """
+    return _random_utf8_str(models.Tag.MAX_LEN_NAME + 1)
+
+def tag_description():
+    """Return a value suitable for the ``Tag.description`` model attribute.
+
+    >>> from elts.models import Tag
+    >>> description = tag_description()
+    >>> isinstance(description, unicode)
+    True
+    >>> len(description) >= 1
+    True
+    >>> len(description) <= Tag.MAX_LEN_DESCRIPTION
+    True
+
+    """
+    return _random_utf8_str(1, models.Tag.MAX_LEN_DESCRIPTION)
+
+def invalid_tag_description():
+    """Return a value suitable for the ``Tag.description`` model attribute.
+
+    >>> from elts.models import Tag
+    >>> description = invalid_tag_description()
+    >>> isinstance(description, unicode)
+    True
+    >>> len(description) == Tag.MAX_LEN_DESCRIPTION + 1
+    True
+
+    """
+    return _random_utf8_str(models.Tag.MAX_LEN_DESCRIPTION + 1)
+
+#-------------------------------------------------------------------------------
 
 class ItemNoteFactory(DjangoModelFactory):
     """Instantiate an ``elts.models.ItemNote`` object.
@@ -165,12 +251,40 @@ class ItemNoteFactory(DjangoModelFactory):
     FACTORY_FOR = models.ItemNote
     author_id = SubFactory(UserFactory)
     item_id = SubFactory(ItemFactory)
-    note_text = FuzzyAttribute(
-        lambda: random_utf8_str(models.Note.MAX_LEN_NOTE_TEXT)
-    )
+    note_text = FuzzyAttribute(lambda: note_note_text()) # pylint: disable=W0108
 
-# FIXME: Dates are not handled nicely. See docstring.
+def note_note_text():
+    """Return a value suitable for the ``Note.note_text`` model attribute.
+
+    >>> from elts.models import Note
+    >>> note = note_note_text()
+    >>> isinstance(note, unicode)
+    True
+    >>> len(note) >= 1
+    True
+    >>> len(note) <= Note.MAX_LEN_NOTE_TEXT
+    True
+
+    """
+    return _random_utf8_str(1, models.Note.MAX_LEN_NOTE_TEXT)
+
+def invalid_note_note_text():
+    """Return a value suitable for the ``Note.note_text`` model attribute.
+
+    >>> from elts.models import Note
+    >>> note = invalid_note_note_text()
+    >>> isinstance(note, unicode)
+    True
+    >>> len(note) == Note.MAX_LEN_NOTE_TEXT + 1
+    True
+
+    """
+    return _random_utf8_str(models.Note.MAX_LEN_NOTE_TEXT + 1)
+
+#-------------------------------------------------------------------------------
+
 class LendFactory(DjangoModelFactory):
+    # pylint: disable=C0301
     """Base attributes for an ``elts.models.Lend`` object.
 
     Using random data for dates and datetimes can produce exceptions. They
@@ -229,16 +343,7 @@ class PastLendFactory(LendFactory):
     """
     # pylint: disable=R0903
     # pylint: disable=W0232
-    #
-    # Variable assignments within factory classes are interpreted as db column
-    # assignments. However, private variables (like ``_lo``) are exempt from
-    # this treatment.
-    _lo = datetime.min
-    _hi = datetime.max
-    out = FuzzyDateTime(
-        datetime(1900, _lo.month, _lo.day, tzinfo = UTC),
-        datetime(2100, _hi.month, _hi.day, tzinfo = UTC)
-    )
+    out = FuzzyAttribute(lambda: lend_out()) # pylint: disable=W0108
 
 class FutureLendFactory(LendFactory):
     """An ``elts.models.Lend`` object whose ``due_out`` attribute is set.
@@ -251,16 +356,7 @@ class FutureLendFactory(LendFactory):
     """
     # pylint: disable=R0903
     # pylint: disable=W0232
-    #
-    # Variable assignments within factory classes are interpreted as db column
-    # assignments. However, private variables (like ``_lo``) are exempt from
-    # this treatment.
-    _lo = date.min
-    _hi = date.max
-    due_out = FuzzyDate(
-        date(1900, _lo.month, _lo.day),
-        date(2100, _hi.month, _hi.day)
-    )
+    due_out = FuzzyAttribute(lambda: lend_due_out()) # pylint: disable=W0108
 
 def random_lend_factory():
     """Return a subclass of LendFactory.
@@ -271,3 +367,89 @@ def random_lend_factory():
 
     """
     return random.choice([PastLendFactory, FutureLendFactory])
+
+def lend_due_out():
+    """Return a value suitable for the ``Lend.due_out`` attribute.
+
+    >>> from datetime import date
+    >>> isinstance(lend_due_out(), date)
+    True
+
+    """
+    lower = date.min
+    upper = date.max
+    return FuzzyDate(
+        date(1900, lower.month, lower.day),
+        date(2100, upper.month, upper.day)
+    ).fuzz()
+
+def lend_due_back():
+    """Return a value suitable for the ``Lend.due_back`` attribute.
+
+    >>> from datetime import date
+    >>> isinstance(lend_due_back(), date)
+    True
+
+    """
+    return lend_due_out()
+
+def lend_out():
+    """Return a value suitable for the ``Lend.out`` attribute.
+
+    >>> from datetime import datetime
+    >>> isinstance(lend_out(), datetime)
+    True
+
+    """
+    lower = datetime.min
+    upper = datetime.max
+    return FuzzyDateTime(
+        datetime(1900, lower.month, lower.day, tzinfo = UTC),
+        datetime(2100, upper.month, upper.day, tzinfo = UTC)
+    ).fuzz()
+
+def lend_back():
+    """Return a value suitable for the ``Lend.back`` attribute.
+
+    >>> from datetime import datetime
+    >>> isinstance(lend_back(), datetime)
+    True
+
+    """
+    return lend_out()
+
+#-------------------------------------------------------------------------------
+
+def _random_integer(lower, upper):
+    """Return a random integer between ``lower`` and ``upper``, inclusive.
+
+    If ``lower >= upper``, return ``lower``.
+
+    >>> _random_integer(0, 0)
+    0
+    >>> _random_integer(5, 5)
+    5
+    >>> _random_integer(5, 0)
+    5
+    >>> _random_integer(0, 5) in range(0, 6)
+    True
+
+    """
+    if(lower >= upper):
+        return lower
+    return random.randint(0, upper - lower) + lower
+
+def _random_utf8_str(min_len = 0, max_len = 0):
+    """Return a string consisting of random UTF-8 characters.
+
+    If ``min_len >= max_len``, return a string exactly ``min_len`` characters
+    long. Otherwise, return a string between ``min_len`` and ``max_len`` chars
+    long, inclusive.
+
+    See also: http://docs.python.org/2/library/functions.html#unichr
+
+    """
+    string = ''
+    for i in range(_random_integer(min_len, max_len)):
+        string += unichr(random.randrange(0, 65535))
+    return string
