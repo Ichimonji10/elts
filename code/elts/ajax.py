be78339b97d9f07a5606e51481917d60a83b2f13
Jeremy Audet <ichimonji10@gmail.com>
2013-12-10 17:20:59 -0500

    Try to call python function with an arg
    Unfortunately, it fails. I don't really know why, either, as the error appears
    to lie within the dajaxice-generated JavaScript.

--- code/elts/ajax.py
+++ code/elts/ajax.py
@@ -2,5 +2,5 @@ from dajaxice.decorators import dajaxice_register
 import json
 
 @dajaxice_register
-def sayhello(request):
-    return json.dumps({'message': 'Hello World'})
+def sayhello(request, arg):
+    return json.dumps({'message': 'Message: {}'.format(arg)})
